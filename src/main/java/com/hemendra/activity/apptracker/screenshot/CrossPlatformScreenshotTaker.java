851b98643dfa3b8f3779fcd11f2be72e47ed1833
hemendra <hemendra.sethi@bipros.com>
2024-08-13 07:58:09 +0530

    Screenshot images are captured and compressed.

--- src/main/java/com/hemendra/activity/apptracker/screenshot/CrossPlatformScreenshotTaker.java
+++ src/main/java/com/hemendra/activity/apptracker/screenshot/CrossPlatformScreenshotTaker.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -46,28 +48,35 @@ public class CrossPlatformScreenshotTaker {
         }
     }
 
-    private BufferedImage captureFullDesktop() throws Exception {
-        Robot robot = new Robot();
-        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-        return robot.createScreenCapture(screenRect);
-    }
-
-    private String getMacOSActiveWindowName() throws Exception {
-        String[] cmd = {"osascript", "-e", "tell application \"System Events\" to name of application processes whose frontmost is true"};
-        Process proc = Runtime.getRuntime().exec(cmd);
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        String line = stdInput.readLine();
-        return line != null ? line : "Unknown";
-    }
-
     private void saveScreenshot(BufferedImage image) throws Exception {
-        String format = "png";
+        String format = "jpg";
         String fileName = getTimestamp() + "." + format;
         File file = new File("screenshots/" + fileName);
         file.getParentFile().mkdirs(); // Create directories if they don't exist
-        ImageIO.write(image, format, file);
-
+        //ImageIO.write(image, format, file);
+        compressImage(image, file, 0.2f);
         log.info("A screenshot was saved as: " + file.getAbsolutePath());
+    }
+
+    public static void compressImage(BufferedImage image, File outputFile, float quality) throws IOException {
+        // Get an ImageWriter for JPEG format
+        ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
+        ImageWriteParam param = writer.getDefaultWriteParam();
+
+        // Set compression quality
+        if (param.canWriteCompressed()) {
+            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            param.setCompressionQuality(quality);  // 0.0 to 1.0 (low to high quality)
+        }
+
+        // Write the compressed image to the output file
+        try (FileOutputStream fos = new FileOutputStream(outputFile);
+             ImageOutputStream ios = ImageIO.createImageOutputStream(fos)) {
+            writer.setOutput(ios);
+            writer.write(null, new IIOImage(image, null, null), param);
+        }
+
+        writer.dispose();
     }
 
     private String getTimestamp() {
